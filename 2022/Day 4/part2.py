9d48702166e3e2744b46c2692ee1820e9cc56366
Antonio Iatonna <ant.iatonna@gmail.com>
2022-12-04 10:23:09 -0500

    Day 4

---
+++ 2022/Day 4/part2.py
@@ -0,0 +1,19 @@
+total = 0
+
+with open('day4Input.txt', 'r') as file:
+    
+    for line in file:
+        pair1, pair2 = line.split(",")
+        
+        pair1Start, pair1End = pair1.split("-")
+        pair2Start, pair2End = pair2.split("-")
+
+        setA = set(i for i in range(int(pair1Start), int(pair1End) + 1))
+        setB = set(i for i in range(int(pair2Start), int(pair2End) + 1))
+
+        if(setA.intersection(setB) != set()):
+            total += 1
+
+    file.close()
+
+    print(total)
